b1f2bbbabb053a6dd26d3eeb39c70b68d08dca0f
izabely <izabelydaluzfernandes@gmail.com>
2025-10-31 16:08:14 -0300

    menu inicial e da 1fase

--- Plup-adventures/Assets/script/Menu.cs
+++ Plup-adventures/Assets/script/Menu.cs
@@ -1,22 +1,43 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-
 public class Menu : MonoBehaviour
 {
 
-    void Start()
-    {
 
-    }
-
-    void Update()
-    {
-
-    }
-
-    public void play()
+    public void StartGame()
     {
         SceneManager.LoadScene(1);
+
+    }
+
+    public void nogame()
+    {
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void ActiveConfig(GameObject go)
+    {
+        go.SetActive(true);
+
+    }
+
+    public void DisableConfig(GameObject go)
+    {
+        go.SetActive(false);
+
+    }
+    public void ActivePause(GameObject go)
+    {
+        Time.timeScale = 0;
+        go.SetActive(true);
+    }
+
+    public void DisablePause(GameObject go)
+    {
+        Time.timeScale = 1;
+        go.SetActive(false);
     }
 }
+
