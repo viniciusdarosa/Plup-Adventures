efccd6728b2ac0a36c437a360dac16144cf840df
Gustavo Budny <gustavosouza1317@gmail.com>
2025-11-02 13:32:33 -0300

    arrumando mais uma vez

--- Plup-adventures/Assets/script/Player.cs
+++ Plup-adventures/Assets/script/Player.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    
+
     private Rigidbody2D rigd; // vou declarar o rigidbory publico para acessar ele 
     public float speed; // vou declarar a velocidade  publico para mudar ela 
 
@@ -59,14 +59,16 @@ public class Player : MonoBehaviour
         }
 
     }
-    //private void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if (collision.gameObject.tag == "tagGround")
-    //    {
-    //        isground = false;
-    //        Debug.Log("esta no chão");
-    //    }
-    //}
 }
+
+//    private void OnCollisionEnter2D(Collision2D collision)
+//    {
+//        if (collision.gameObject.tag == "tagGround")
+//        {
+//            isground = false;
+//            Debug.Log("esta no chão");
+//        }
+//    }
+//}
 
 
